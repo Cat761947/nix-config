37c9e9e3012bea583670c2f62950f661f2899a4f
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-06-07 19:49:53 -0600

    Added custom nix-darwin preferences & removed incorrect preference

--- modules/parts/nix-darwin/preferences.nix
+++ modules/parts/nix-darwin/preferences.nix
@@ -1,23 +1,57 @@
 {
   flake.modules.darwin.preferences = {
+    
     # Firewall
     networking.applicationFirewall = {
       enable = true;
       allowSignedApp = false;
       allowSigned = false;
     };
+    
+    # Language & Region
+    system.defaults.CustomUserPreferences.NSGlobalDomain = {
+      AppleICUDateFormatStrings = { "1" = "dd/MM/y"; };
+      AppleICUNumberSymbols = { "0" = "."; "1" = " "; "10" = "."; "17" = " "; };
+      AppleLiveTextEnabled = false;
+    };
+    
+    # Accessibility
+    system.defaults.CustomUserPreferences = {
+      "com.apple.universalaccess" = {
+        differentiateWithoutColor = true;
+	showToolbarButtonShapes = true;
+	showWindowTitlebarIcons = true;
+      };
+    
+      "com.apple.Accessibility".DifferentiateWithoutColor = 1;
+    };
 
     # Appearance
-    system.defaults.NSGlobalDomain = {
-      AppleInterfaceStyle = "Dark";
-      AppleIconAppearanceTheme = "RegularDark";
-      AppleShowScrollBars = "Always";
-      AppleScrollerPagingBehavior = true;
+    system.defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleIconAppearanceTheme = "RegularDark";
+        AppleShowScrollBars = "Always";
+        AppleScrollerPagingBehavior = true;
+      };
+    
+      CustomUserPreferences = {
+        NSGlobalDomain.NSGlassDiffusionSetting = true;
+      };
     };
 
     # Menu Bar
     system.defaults = {
       controlcenter.NowPlaying = false;
+      
+      CustomUserPreferences = {
+        NSGlobalDomain = {
+	  AppleMenuBarVisibleInFullscreen = true;
+	  SLSMenuBarUseBlurredAppearance = true;
+	};
+
+        "com.apple.controlcenter".AutoHideMenuBarOption = 3;
+      };
     };
 
     # Desktop & Dock
@@ -44,6 +78,49 @@
 	EnableTopTilingByEdgeDrag = false;
 	EnableTilingOptionAccelerator = false;
       };
+
+      CustomUserPreferences = {
+        NSGlobalDomain = {
+	  AppleActionOnDoubleClick = "None";
+	  AppleWindowTabbingMode = "always";
+	  NSCloseAlwaysConfirmsChanges = true;
+	};
+	
+	"com.apple.dock" = {
+	  enterMissionControlByTopWindowDrag = false;
+	  wvous-tl-modifier = 131072;
+        };
+      };
+    };
+    
+    # Spotlight
+    system.defaults.CustomUserPreferences = {
+      "com.apple.assistant.support"."Search Queries Data Sharing Status" = 2;
+      
+      "com.apple.Spotlight".EnabledPreferenceRules = [
+        "Custom.relatedContents"
+	"com.apple.AppStore"
+	"com.apple.iBooksX"
+	"com.apple.iCal"
+	"com.apple.clock"
+	"com.apple.AddressBook"
+	"com.apple.Dictionary"
+	"com.apple.mail"
+	"com.apple.Notes"
+	"com.apple.Photos"
+	"com.apple.podcasts"
+        "ch.protonmail.drive"
+        "com.apple.reminders"
+	"com.apple.Safari"
+	"com.apple.shortcuts"
+	"com.apple.systempreferences"
+	"com.apple.tips"
+	"com.apple.VoiceMemos"
+	"System.files"
+	"System.folders"
+	"System.iphoneApps"
+	"System.menuItems"
+      ];
     };
 
     # Sound
@@ -51,9 +128,6 @@
       ".GlobalPreferences"."com.apple.sound.beep.sound" = "/System/Library/Sounds/Funk.aiff";
       NSGlobalDomain."com.apple.sound.beep.feedback" = 1;
     };
-
-    # Lock Screen
-    system.defaults.screensaver.askForPasswordDelay = 0;
 
     # Keyboard
     system.defaults = {
@@ -67,7 +141,9 @@
 	NSAutomaticPeriodSubstitutionEnabled = false;
 	NSAutomaticQuoteSubstitutionEnabled = false;
       };
+      
       hitoolbox.AppleFnUsageType = "Do Nothing";
+      CustomUserPreferences.NSGlobalDomain.AppleKeyboardUIMode = 2;
     };
 
     # Finder
@@ -86,6 +162,11 @@
 	ShowStatusBar = true;
 	ShowPathbar = true;
       };
+
+      CustomUserPreferences."com.apple.finder"."NSWindowTabbingShoudShowTabBarKey-com.apple.finder.TBrowserWindow" = true;
     };
+
+    # TextEdit
+    system.defaults.CustomUserPreferences."/Users/cat_761947/Library/Containers/com.apple.TextEdit/Data/Library/Preferences/com.apple.TextEdit.plist".RichText = 0;
   };
 }
