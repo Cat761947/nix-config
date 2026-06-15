3e52e758b7d7f78ac566542c2718f0340e0df8e1
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-06-14 19:58:38 -0600

    Added setup packages

--- modules/extras/packages/setup-packages.nix
+++ modules/extras/packages/setup-packages.nix
@@ -1,1 +1,8 @@
-{}
+{ inputs, ... }: {
+  flake.packages.aarch64-darwin.darwin-rebuild = inputs.nix-darwin.packages.aarch64-darwin.darwin-rebuild;
+  flake.packages.aarch64-darwin.darwin-uninstaller = inputs.nix-darwin.packages.aarch64-darwin.darwin-uninstaller;
+
+  perSystem = { inputs', ... }: {
+    packages.home-manager = inputs'.home-manager.packages.home-manager;
+  };
+}
