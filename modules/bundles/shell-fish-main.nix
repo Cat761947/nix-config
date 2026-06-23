372a5fd7a2e5191fa5350719fcfa0522c4e8023f
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-06-23 01:22:55 -0600

    improved setting SHELL var inside shell-fish-main bundle

--- modules/bundles/shell-fish-main.nix
+++ modules/bundles/shell-fish-main.nix
@@ -1,5 +1,5 @@
 { self, lib, ... }: {
-  flake.wrappers.shell-fish-main = { pkgs, wlib, ... }:
+  flake.wrappers.shell-fish-main = { pkgs, wlib, config, ... }:
   let selfpkgs = self.packages.${pkgs.stdenv.hostPlatform.system}; in {
     imports = with self.wrapperModules; [ fish wlib.modules.default ];
     
@@ -15,7 +15,7 @@
       selfpkgs.tmux
     ];
 
-    configFile.content = "${self.wrappers.fish.configFile.content} \n set SHELL (which fish) \n starship init fish | source";
+    configFile.content = "${self.wrappers.fish.configFile.content} \n set SHELL ${placeholder config.outputName}/${config.binDir}/${config.binName} \n starship init fish | source";
 
     # Temporary fix for MacOS where AppleGit takes higher priority than selfpkgs.git
     shellAliases = if pkgs.stdenv.isDarwin then { git = lib.getExe selfpkgs.git; } else {};
