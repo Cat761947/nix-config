5e99e5f0c84e1890823dc39fcea6558ecf97ab9a
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-06-28 15:32:45 -0600

    Added vim-tmux-navigator to nvim

--- modules/packages/neovim.nix
+++ modules/packages/neovim.nix
@@ -36,6 +36,47 @@
         };
       };
 
+      lazy = {
+        enable = true;
+        plugins.vim-tmux-navigator = {
+          package = pkgs.vimPlugins.vim-tmux-navigator;
+          cmd = [
+            "TmuxNavigateLeft"
+            "TmuxNavigateDown"
+            "TmuxNavigateUp"
+            "TmuxNavigateRight"
+            "TmuxNavigatePrevious"
+            "TmuxNavigatorProcessList"
+          ];
+          keys =
+            map (x: {
+              key = x.key;
+              action = x.action;
+              mode = "n";
+            }) [
+              {
+                key = "<c-h>";
+                action = "<cmd><C-U>TmuxNavigateLeft<cr>";
+              }
+              {
+                key = "<c-j>";
+                action = "<cmd><C-U>TmuxNavigateDown<cr>";
+              }
+              {
+                key = "<c-k>";
+                action = "<cmd><C-U>TmuxNavigateUp<cr>";
+              }
+              {
+                key = "<c-l>";
+                action = "<cmd><C-U>TmuxNavigateRight<cr>";
+              }
+              {
+                key = "<c-\\>";
+                action = "<cmd><C-U>TmuxNavigatePrevious<cr>";
+              }
+            ];
+        };
+      };
       autocomplete.blink-cmp.enable = true;
 
       opts.shiftwidth = 2;
