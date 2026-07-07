09a8ba6b246527e38236b77d23c83b9d9ba5f3d0
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-07-06 22:46:11 -0600

    Switched from nixpkgs-* to nixos-* & flake.lock update

--- flake.nix
+++ flake.nix
@@ -2,9 +2,8 @@
   description = "Nix configuration for all my hosts";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs-stable-darwin.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:denful/import-tree";
--- modules/features/nix-darwin/packages-utility.nix
+++ modules/features/nix-darwin/packages-utility.nix
@@ -1,11 +1,10 @@
-{inputs, ...}: {
+{
   flake.modules.darwin.feature-packages-utility = {pkgs, ...}: {
     nixpkgs.config.allowUnfree = true;
     environment.systemPackages = with pkgs; [
       libreoffice-bin
       discord
-      # Switch to pkgs on next flake.lock update
-      inputs.nixpkgs-stable-darwin.legacyPackages.${pkgs.stdenv.hostPlatform.system}.audacity
+      audacity
     ];
 
     homebrew.casks = [
