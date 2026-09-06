343eb2f7c5a82377646ce8a9784fa950c619d352
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-09-06 13:36:18 -0600

    nix-wrapper-modules moved to nix-community

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    nix-wrapper-modules.url = "github:nix-community/nix-wrapper-modules";
   };
 
   outputs = inputs:
