877e0e545f5f4bfa5c0fb957b8ebe4d3bae1fa92
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-09-05 21:46:30 -0600

    Swapped lualine filename and lsp status

--- modules/packages/neovim/lua/lua/plugins/lualine.lua
+++ modules/packages/neovim/lua/lua/plugins/lualine.lua
@@ -15,11 +15,11 @@ local config = {
     lualine_b = { "diagnostics" },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = {
-      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+    lualine_y = { "lsp_status" },
+    lualine_z = {
+      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 }, colored = false },
       { "filename", padding = { left = 0, right = 1 } },
     },
-    lualine_z = { "lsp_status" },
   },
 }
 config.inactive_sections = config.sections
