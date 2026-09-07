3065e1044c2093e6c26c14fe563b43326e443753
Cat_761947 <cat_761947@noreply.codeberg.org>
2026-09-07 15:13:26 -0600

    Enabled catppuccin integrations

--- modules/packages/neovim/lua/lua/plugins/catppuccin.lua
+++ modules/packages/neovim/lua/lua/plugins/catppuccin.lua
@@ -2,6 +2,18 @@ require("lz.n").load({
   "catppuccin-nvim",
   colorscheme = "catppuccin-nvim",
   after = function()
-    require("catppuccin").setup({ flavour = require("nix-info").settings.catppuccinFlavour })
+    require("catppuccin").setup({
+      flavour = require("nix-info").settings.catppuccinFlavour,
+      default_integrations = false,
+      integrations = {
+        blink_cmp = {
+          style = "solid",
+          enabled = true,
+        },
+        harpoon = true,
+        noice = true,
+        telescope = true,
+      },
+    })
   end,
 })
